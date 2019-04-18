9121c58cf1a53e99102fbe7858c4a4d583785619
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-04-18 15:53:32 -0700

    17. Add Signout Functionality, Clear User Action

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import {createStore} from "redux";
 import {Provider, connect} from "react-redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import rootReducer from "./reducers";
-import {setUser} from "./actions";
+import {setUser, clearUser} from "./actions";
 
 const store = createStore(rootReducer, composeWithDevTools());
 
@@ -34,6 +34,9 @@ class Root extends React.Component {
         console.log(user);
         this.props.setUser(user);
         this.props.history.push("/");
+      } else {
+        this.props.history.push("/login");
+        this.props.clearUser(); // action to clear user from global state
       }
     });
   }
@@ -58,7 +61,7 @@ const mapStateToProps = state => ({
 const RootWithAuth = withRouter(
   connect(
     mapStateToProps,
-    {setUser}
+    {setUser, clearUser}
   )(Root)
 ); // puts the history obj in Root component
 
