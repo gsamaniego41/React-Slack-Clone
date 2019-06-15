53b9dcf50130340090bff622b71fbed2dad5b4ad
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-06-14 23:41:28 -0700

    Creates MessagesForm component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,20 +1,20 @@
 import React, {Component} from "react";
 import {Segment, Comment} from "semantic-ui-react";
-import MessagesHeader from './MessagesHeader';
+import MessagesHeader from "./MessagesHeader";
+import MessagesForm from "./MessagesForm";
 
 class Messages extends Component {
   render() {
     return (
       <>
         <MessagesHeader />
-          <Segment>
-            <Comment.Group className="messages">
-              {/* Messages */}
-            </Comment.Group>
-          </Segment>
+        <Segment>
+          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+        </Segment>
         <MessagesForm />
       </>
-    )
+    );
+  }
 }
 
 export default Messages;
