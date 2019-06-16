eaeb43a01fe5d4c72a04acc5f00422d7e2d95637
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-06-16 16:44:18 -0700

    Displays messages, creates message component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -4,22 +4,66 @@ import firebase from "../../firebase";
 
 import MessagesHeader from "./MessagesHeader";
 import MessagesForm from "./MessagesForm";
+import Message from "./Message";
 
 class Messages extends Component {
   state = {
     messagesRef: firebase.database().ref("messages"),
+    messages: [],
+    messagesLoading: true,
     channel: this.props.currentChannel,
     currentUser: this.props.currentUser
   };
 
+  componentDidMount() {
+    const {channel, currentUser} = this.state;
+
+    if (channel && currentUser) {
+      this.addListeners(channel.id);
+    }
+  }
+
+  addListeners = channelId => {
+    this.addMessageListener(channelId);
+  };
+
+  addMessageListener = channelId => {
+    let loadedMessages = [];
+    this.state.messagesRef.child(channelId).on("child_added", snap => {
+      loadedMessages.push(snap.val());
+      console.log("loadedMessages", loadedMessages);
+      this.setState({
+        messages: loadedMessages,
+        messagesLoading: false
+      });
+    });
+  };
+
+  displayMessages = messages => {
+    return (
+      messages.length > 0 &&
+      messages.map(message => {
+        return (
+          <Message
+            key={message.timestamp}
+            message={message}
+            user={this.state.currentUser}
+          />
+        );
+      })
+    );
+  };
+
   render() {
-    const {messagesRef, channel, currentUser} = this.state;
+    const {messagesRef, messages, channel, currentUser} = this.state;
 
     return (
       <>
         <MessagesHeader />
         <Segment>
-          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+          <Comment.Group className="messages">
+            {this.displayMessages(messages)}
+          </Comment.Group>
         </Segment>
         <MessagesForm
           messagesRef={messagesRef}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import {combineReducers} from "redux";
 import * as actionTypes from "../actions/types";
-import {root} from "postcss";
 
 const initialUserState = {
   currentUser: null,
